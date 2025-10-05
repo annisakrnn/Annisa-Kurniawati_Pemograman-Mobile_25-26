87679b73ffb7e4aa7f0514ea741327540df35b6c
Annisa Kurniawati <annisak0708@gmail.com>
2025-10-05 11:13:12 +0700

    P2_Langkah 1

--- week_6/layout_flutter/lib/main.dart
+++ week_6/layout_flutter/lib/main.dart
@@ -50,11 +50,32 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter layout: Ratnasari 2241720007',
+      title: 'Flutter layout: Anniss Kurniawati 2341720070',
       home: Scaffold(
         appBar: AppBar(title: const Text('My App')),
         body: Column(children: [titleSection]),
       ),
+    );
+  }
+
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
