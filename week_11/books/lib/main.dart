6732ac9354d32684ff60911cc07b4780f585aac6
Annisa Kurniawati <annisak0708@gmail.com>
2025-11-06 15:26:34 +0700

    Praktikum 2 dan 3

--- week_11/books/lib/main.dart
+++ week_11/books/lib/main.dart
@@ -44,16 +44,31 @@ class _FuturePageState extends State<FuturePage> {
             ElevatedButton(
               child: const Text('GO!'),
               onPressed: () {
-                setState(() {});
-                getData()
+                //Langkah 3 Praktikum 3
+                getNumber()
                     .then((value) {
-                      result = value.body.toString().substring(0, 450);
-                      setState(() {});
+                      setState(() {
+                        result = value.toString();
+                      });
+                      //langkah 6 praktikum 3
                     })
-                    .catchError((error) {
+                    .catchError((e) {
                       result = 'An error occurred';
-                      setState(() {});
                     });
+                //Langkah 5 Praktikum 1
+                //   setState(() {});
+                //   getData()
+                //       .then((value) {
+                //         result = value.body.toString().substring(0, 450);
+                //         setState(() {});
+                //       })
+                //       .catchError((error) {
+                //         result = 'An error occurred';
+                //         setState(() {});
+                //       });
+                // },
+                //Langkah 3 Praktikum 2
+                // count();
               },
             ),
             const Spacer(),
@@ -67,10 +82,56 @@ class _FuturePageState extends State<FuturePage> {
     );
   }
 
+  late Completer completer;
+  Future getNumber() {
+    completer = Completer<int>();
+    calculate();
+    return completer.future;
+  }
+
+  calculate() async {
+    //langkah 3 praktikum 3
+    try {
+      await Future.delayed(const Duration(seconds: 5));
+      completer.complete(42);
+      //throw Exception();
+    } catch (e) {
+      completer.completeError({});
+    }
+    //langkah 2 praktikum 3
+    // await Future.delayed(const Duration(seconds: 5));
+    // completer.complete(42);
+  }
+
   Future<Response> getData() async {
     const authority = 'www.googleapis.com';
     const path = '/books/v1/volumes/0Xhtf6-BAqcC';
     Uri url = Uri.https(authority, path);
     return http.get(url);
+  }
+
+  Future<int> returnOneAsync() async {
+    await Future.delayed(const Duration(seconds: 3));
+    return 1;
+  }
+
+  Future<int> returnTwoAsync() async {
+    await Future.delayed(const Duration(seconds: 3));
+    return 2;
+  }
+
+  Future<int> returnThreeAsync() async {
+    await Future.delayed(const Duration(seconds: 3));
+    return 3;
+  }
+
+  Future count() async {
+    int total = 0;
+    total += await returnOneAsync();
+    total += await returnTwoAsync();
+    total += await returnThreeAsync();
+    setState(() {
+      result = total.toString();
+    });
   }
 }
