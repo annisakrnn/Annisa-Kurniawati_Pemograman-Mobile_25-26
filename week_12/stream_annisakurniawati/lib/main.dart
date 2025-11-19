a575e1c1d6df845b6b6e7286b38f901e5802b526
Annisa Kurniawati <annisak0708@gmail.com>
2025-11-19 12:00:56 +0700

    Praktikum 5

--- week_12/stream_annisakurniawati/lib/main.dart
+++ week_12/stream_annisakurniawati/lib/main.dart
@@ -32,9 +32,10 @@ class StreamHomePage extends StatefulWidget {
 class _StreamHomePageState extends State<StreamHomePage> {
   Color bgColor = Colors.blueGrey;
   late ColorStream colorStream;
+
   int lastNumber = 0;
   late NumberStream numberStream;
-  late StreamController numberStreamController;
+  late StreamController<int> numberStreamController;
   late StreamTransformer transformer;
   late StreamSubscription subscription;
   late StreamSubscription subscription2;
@@ -42,24 +43,37 @@ class _StreamHomePageState extends State<StreamHomePage> {
 
   @override
   void initState() {
-    super.initState();
     colorStream = ColorStream();
     numberStream = NumberStream();
     numberStreamController = numberStream.controller;
-    Stream stream = numberStreamController.stream;
+    Stream stream = numberStreamController.stream.asBroadcastStream();
     subscription = stream.listen((event) {
       setState(() {
-        values += '$event - ';
-        subscription.onError((error) {
-          setState(() {
-            lastNumber = -1;
-          });
-        });
-        subscription2 = stream.listen((event) {
-          setState(() {
-            values += event;
-          });
-        });
+        // lastNumber = event;
+        values += '$event -';
+      });
+    });
+    subscription2 = stream.listen((event) {
+      setState(() {
+        // values = event;
+        values += '$event -';
+      });
+    });
+  }
+
+  void changeColor() async {
+    colorStream.getColors().listen((eventColor) {
+      setState(() {
+        bgColor = eventColor;
+      });
+    });
+    subscription.onDone(() {
+      print("OnDone was Called");
+    });
+
+    subscription.onError((error) {
+      setState(() {
+        lastNumber = -1;
       });
     });
   }
@@ -69,41 +83,6 @@ class _StreamHomePageState extends State<StreamHomePage> {
     numberStreamController.close();
     subscription.cancel();
     super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Stream Annisa')),
-      body: SizedBox(
-        width: double.infinity,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(lastNumber.toString()),
-
-            ElevatedButton(
-              onPressed: () => addRandomNumber(),
-              child: const Text('New Random Number'),
-            ),
-
-            ElevatedButton(
-              onPressed: () => stopStream(),
-              child: const Text('Stop Subscription'),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  void changeColor() async {
-    colorStream.getColors().listen((eventColor) {
-      setState(() {
-        bgColor = eventColor;
-      });
-    });
   }
 
   void addRandomNumber() {
@@ -122,5 +101,32 @@ class _StreamHomePageState extends State<StreamHomePage> {
 
   void stopStream() {
     numberStreamController.close();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Stream Annisa')),
+      body: SizedBox(
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(values),
+
+            ElevatedButton(
+              onPressed: () => addRandomNumber(),
+              child: const Text('New Random Number'),
+            ),
+
+            ElevatedButton(
+              onPressed: () => stopStream(),
+              child: const Text('Stop Subscription'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
